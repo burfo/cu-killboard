d259e6d6c02fd48f2673241ecd7b5d50a3274df0
burfo <lziobro@gmail.com>
2014-09-09 01:37:19 -0500

    Bunch of functional updates... Drop the use of sorttable.js and sortable tables.  Instead, sort the data when its loaded and show it in descending order.  There are now 3 display "tabs" (Kills, Deaths, K/D).

--- script.js
+++ script.js
@@ -5,37 +5,69 @@ CU.KillBoard = (function ()
 {
     function KB()
     {
-        this.startDate = this.sampleStartDate;
-        this.endDate = this.sampleEndDate;
-        this.killers = {};
+        this.startDate = "";
+        this.endDate = "";
+        this.listQtyLimit = 20;
     };
     
     KB.prototype.apiUrl = "http://chat.camelotunchained.com:8000/api/kills";
-    KB.prototype.sampleStartDate = new Date('2014-07-17T21:45:00.000Z');
-    KB.prototype.sampleEndDate = new Date('2014-07-17T22:15:00.000Z');
+    KB.prototype.sampleStartDate = new Date('2014-09-05T04:30:00.000Z');
+    KB.prototype.sampleEndDate = new Date('2014-09-06T04:30:00.000Z');
     
     KB.prototype.init = function ()
     {
-        $("#errorRetry").click($.proxy(function()
+        $("#errorRetry").click($.proxy(function ()
         {
             this.initialLoad();
         }, this));
-        this.initialLoad();
         
+        $("#btnKills").click($.proxy(function ()
+        {
+            $("#killsGrid").show();
+            $("#deathsGrid").hide();
+            $("#kdGrid").hide();
+        }, this));
+        
+        $("#btnDeaths").click($.proxy(function ()
+        {
+            $("#deathsGrid").show();
+            $("#killsGrid").hide();
+            $("#kdGrid").hide();
+        }, this));
+        
+        $("#btnKD").click($.proxy(function ()
+        {
+            $("#kdGrid").show();
+            $("#deathsGrid").hide();
+            $("#killsGrid").hide();
+        }, this));
+        
+        $("#buttons").buttonset();
+        
+        
+        this.initialLoad();
+        $("#btnKills").click().tooltip({content: "test" });
     };
     
     KB.prototype.initialLoad = function ()
     {
-        $("#loader").show();
+        $("#allData").hide();
         $("#error").hide();
+        $("#loader").show();
+        this.refreshData();
+    };
+    
+    KB.prototype.refreshData = function ()
+    {
         $.ajax({
             method: 'GET',
-            url: this.apiUrl + "?start=" + this.startDate.toISOString(),
+            url: this.apiUrl + (this.startDate ? "?start=" + this.startDate.toISOString() : ""),
             dataType: 'json'
         }).done($.proxy(function(data) {
-            this.receiveIncomingData(data);
+            var allData = this.receiveIncomingData(data);
+            this.displayData(allData);
             $("#loader").fadeOut();
-            this.displayData();
+            window.setTimeout($.proxy(this.refreshData, this), 60000);
         }, this)).fail($.proxy(function(jqXHR, textStatus, errorThrown) {
             $("#loader").hide();
             $("#error").show();
@@ -45,6 +77,8 @@ CU.KillBoard = (function ()
     KB.prototype.receiveIncomingData = function (killData)
     {
         var name;
+        var killers = {};
+        var killersAry = [];
         
         for (var index = 0; index < killData.length; index++)
         {
@@ -54,12 +88,12 @@ CU.KillBoard = (function ()
             {
                 name = kill.victim.name;
                 
-                if (!this.killers[name])
+                if (!killers[name])
                 {
-                    this.killers[name] = new CU.Killer(name, kill.victim.faction, kill.victim.race);
+                    killers[name] = new CU.Killer(name, kill.victim.faction);
                 }
                 
-                this.killers[name].deaths++;
+                killers[name].deaths++;
             }
             
             //don't tally a kill if the victim killed theirself
@@ -67,71 +101,123 @@ CU.KillBoard = (function ()
             {
                 name = kill.killer.name;
                 
-                if (!this.killers[name])
+                if (!killers[name])
                 {
-                    this.killers[name] = new CU.Killer(name, kill.killer.faction, kill.killer.race);
+                    killers[name] = new CU.Killer(name, kill.killer.faction);
                 }
                 
-                this.killers[name].kills++;
+                killers[name].kills++;
             }
-        }
-    };
-    
-    KB.prototype.displayData = function ()
-    {
-        var tableBody = $("#killsGrid > tbody:last");
-        var key;
-        for (key in this.killers)
-        {
-            if (!this.killers.hasOwnProperty(key))
-            {
-                continue;
-            }
-         
-            var killer = this.killers[key];
-            
-            if (!killer)
-            {
-                continue;
-            }
-         
-         
-         
-            var row = document.createElement("tr");
-            var rowHtml = "<td>" + killer.name + "</td>";
-            rowHtml += "<td>" + killer.race + "</td>";
-            rowHtml += "<td>" + killer.kills + "</td>";
-            
-            var kd = killer.killDeathRatio();
-            if (kd === Number.POSITIVE_INFINITY)
-            {
-                kd = "&infin;";
-            }
-            else
-            {
-                kd = +kd.toFixed(2);
-            }
-            
-            rowHtml += "<td>" + kd + "</td>";
-            
-            if (killer.faction == CU.Factions.Arthurian)
-            {
-                $(row).addClass("Arthurian");
-            }
-            else if (killer.faction == CU.Factions.TDD)
-            {
-                $(row).addClass("TDD");
-            }
-            else if (killer.faction == CU.Factions.Viking)
-            {
-                $(row).addClass("Viking");
-            }
-            
-            $(tableBody).append(row);
-            $(row).append(rowHtml);
-            
         }
         
+        for (key in killers)
+        {
+            if (!killers.hasOwnProperty(key))
+            {
+                continue;
+            }
+            
+            killersAry.push(killers[key]);
+        }
+        
+        return killersAry;
+    };
+    
+    KB.prototype.displayData = function (killers)
+    {
+        
+        /** Kills **/
+        var tableBody = $("#killsGrid > tbody:last");
+        tableBody.empty();
+        var killsEntries = killers.sort(function (a,b)
+        {
+            return b.kills - a.kills;
+        }).slice(0, this.listQtyLimit);
+        
+        for (var idx = 0; idx < killsEntries.length; idx++)
+        {
+            var killer = killsEntries[idx];
+            var row = document.createElement("tr");
+            $(tableBody).append(row);
+            row = $(row);
+            row.append(document.createElement("td"));
+            var name = CU.truncateString(killer.name);
+            row.append($(document.createElement("td")).text(name));
+            row.append($(document.createElement("td")).text(killer.kills));
+            row.addClass(killer.faction);
+            row.tooltip(
+            {
+                content: this.killerTooltip(killer),
+                items: "tr",
+                track: true
+            });
+        }
+        
+        /** K/D **/
+        tableBody = $("#kdGrid > tbody:last");
+        tableBody.empty();
+        var kdEntries = killers.sort(function (a,b)
+        {
+            var comp = b.killDeathRatio() - a.killDeathRatio();
+            //if k/d is equal, secondary sort on # kills
+            if (comp === 0)
+            {
+                comp = b.kills - a.kills;
+            }
+            return comp;
+        }).slice(0, this.listQtyLimit);
+        
+        for (var idx = 0; idx < kdEntries.length; idx++)
+        {
+            var killer = kdEntries[idx];
+            var row = document.createElement("tr");
+            $(tableBody).append(row);
+            row = $(row);
+            row.append(document.createElement("td"));
+            row.append($(document.createElement("td")).text(CU.truncateString(killer.name)));
+            var kdDisplay = +killer.killDeathRatio().toFixed(2);
+            row.append($(document.createElement("td")).text(kdDisplay));
+            row.addClass(killer.faction);
+            row.tooltip(
+            {
+                content: this.killerTooltip(killer),
+                items: "tr",
+                track: true
+            });
+        }
+        
+        /** Deaths **/
+        var tableBody = $("#deathsGrid > tbody:last");
+        tableBody.empty();
+        var killsEntries = killers.sort(function (a,b)
+        {
+            return b.deaths - a.deaths;
+        }).slice(0, this.listQtyLimit);
+        
+        for (var idx = 0; idx < killsEntries.length; idx++)
+        {
+            var killer = killsEntries[idx];
+            var row = document.createElement("tr");
+            $(tableBody).append(row);
+            row = $(row);
+            row.append(document.createElement("td"));
+            row.append($(document.createElement("td")).text(CU.truncateString(killer.name)));
+            row.append($(document.createElement("td")).text(killer.deaths));
+            row.addClass(killer.faction);
+            row.tooltip(
+            {
+                content: this.killerTooltip(killer),
+                items: "tr",
+                track: true
+            });
+        }
+        
+        $("#allData").show();
+    };
+    
+    KB.prototype.killerTooltip = function (killer)
+    {
+        return "<b>" + CU.truncateString(killer.name) + "</b><br />Kills: " + killer.kills + "<br />Deaths: " + killer.deaths + "<br />K/D: " + (+killer.killDeathRatio().toFixed(2)) + "<br />" + killer.faction;
     };
     
     return KB;
@@ -139,33 +225,16 @@ CU.KillBoard = (function ()
 
 CU.Killer = (function ()
 {
-    function Killer(name, faction, race)
+    function Killer(name, faction)
     {
         this.name = name;
         this.kills = 0;
         this.deaths = 0;
         
-        faction = faction.toString().toLowerCase();
-        if (faction == 3 || faction == "authurian")
-        {
-            this.faction = CU.Factions.Arthurian;
-        }
-        else if (faction == 1 || faction == "tdd")
-        {
-            this.faction = CU.Factions.TDD;
-        }
-        else if (faction == 2 || faction == "viking")
-        {
-            this.faction = CU.Factions.Viking;
-        }
-        else
-        {
-            this.faction = faction;
-        }
-        
-        //todo race
-        this.race = "tbd";
-        
+        if (faction == 3) this.faction = CU.Factions.Arthurian;
+        else if (faction == 1) this.faction = CU.Factions.TDD;
+        else if (faction == 2) this.faction = CU.Factions.Viking;
+        else this.faction = CU.Factions.Factionless;
     };
     
     Killer.prototype.killDeathRatio = function ()
@@ -190,9 +259,15 @@ CU.Factions =
 {
     Arthurian: "Arthurian",
     TDD: "TDD",
-    Viking: "Viking"
+    Viking: "Viking",
+    Factionless: "Factionless"
 };
 if ((typeof Object.freeze) === "function") Object.freeze(CU.Factions);
+
+CU.truncateString = function (str, len)
+{
+    return str.length > len ? str.substring(0, len - 1) + "&infin;" : str;
+};
 
 $(function() {
     kb = new CU.KillBoard();
